080e2ccf980b933234dfb20b1012fa18535033a0
indecisiveuser <lila.schweinfurth@gmail.com>
2025-12-11 17:23:37 -0500

    first draft; appears buggy and slow

--- src/main/java/net/preibisch/mvrecon/fiji/spimdata/explorer/ViewSetupExplorerPanel.java
+++ src/main/java/net/preibisch/mvrecon/fiji/spimdata/explorer/ViewSetupExplorerPanel.java
@@ -82,6 +82,7 @@ import net.preibisch.mvrecon.fiji.spimdata.SpimData2;
 import net.preibisch.mvrecon.fiji.spimdata.XmlIoSpimData2;
 import net.preibisch.mvrecon.fiji.spimdata.explorer.bdv.ScrollableBrightnessDialog;
 import net.preibisch.mvrecon.fiji.spimdata.explorer.popup.AnalyzeErrorPopup;
+import net.preibisch.mvrecon.fiji.spimdata.explorer.popup.AnalyzeOverlapCrossCorrelationPopup;
 import net.preibisch.mvrecon.fiji.spimdata.explorer.popup.ApplyTransformationPopup;
 import net.preibisch.mvrecon.fiji.spimdata.explorer.popup.BDVPopup;
 import net.preibisch.mvrecon.fiji.spimdata.explorer.popup.BakeManualTransformationPopup;
@@ -724,6 +725,7 @@ public class ViewSetupExplorerPanel< AS extends SpimData2 > extends FilteredAndG
 		popups.add( new LabelPopUp( " Interest Points" ) );
 		popups.add( new InterestPointsExplorerPopup() );
 		popups.add( new AnalyzeErrorPopup() );
+		popups.add( new AnalyzeOverlapCrossCorrelationPopup() );
 		popups.add( new RemoveDetectionsPopup() );
 		popups.add( new VisualizeDetectionsPopup() );
 		popups.add( new Separator() );
