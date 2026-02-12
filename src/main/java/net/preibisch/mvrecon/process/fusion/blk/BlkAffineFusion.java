e0bf8ff25431751e3b65d3b63b6f118213669186
tpietzsch <tobias.pietzsch@gmail.com>
2026-02-12 14:22:11 +0100

    clean up

--- src/main/java/net/preibisch/mvrecon/process/fusion/blk/BlkAffineFusion.java
+++ src/main/java/net/preibisch/mvrecon/process/fusion/blk/BlkAffineFusion.java
@@ -9,12 +9,12 @@
  * it under the terms of the GNU General Public License as
  * published by the Free Software Foundation, either version 2 of the
  * License, or (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public
  * License along with this program.  If not, see
  * <http://www.gnu.org/licenses/gpl-2.0.html>.
@@ -137,7 +137,7 @@ public class BlkAffineFusion
 
 			//throw new UnsupportedOperationException( "BlkAffineFusion: Fusion method not supported (yet)." );
 			IOFunctions.println( "BlkAffineFusion: Fusion method not supported (yet). Falling back to LazyAffineFusion." );
-			return BlockSupplier.of( 
+			return BlockSupplier.of(
 					Views.zeroMin( LazyAffineFusion.init( converter, imgloader, viewIds, viewRegistrations, viewDescriptions, fusionType, interpolationMethod, intensityAdjustmentModels, fusionInterval, type, blockSize ) ) );
 		}
 
@@ -279,7 +279,7 @@ public class BlkAffineFusion
 				.tile( 32 );
 
 		System.out.println( Util.printInterval( new FinalInterval( fusionInterval.dimensionsAsLongArray() ) ) );
-		
+
 		return blocks;
 		//return BlockAlgoUtils.cellImg( blocks, fusionInterval.dimensionsAsLongArray(), blockSize );
 	}
