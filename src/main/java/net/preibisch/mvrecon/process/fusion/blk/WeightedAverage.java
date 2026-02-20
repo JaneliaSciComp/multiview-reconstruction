a6973a6e4c53a990f33c3fd0de6fabacd30fb3fc
tpietzsch <tobias.pietzsch@gmail.com>
2026-02-20 13:30:22 +0100

    WIP: testing pieces incrementally

--- src/main/java/net/preibisch/mvrecon/process/fusion/blk/WeightedAverage.java
+++ src/main/java/net/preibisch/mvrecon/process/fusion/blk/WeightedAverage.java
@@ -38,6 +38,8 @@ import net.imglib2.type.numeric.real.FloatType;
 import net.imglib2.util.Cast;
 import net.imglib2.util.Intervals;
 
+public
+// TODO: revert to package after testing is done...
 class WeightedAverage
 {
 	public static BlockSupplier< FloatType > of(
