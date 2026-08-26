6b78587b751fda2f9d5f2934468d41edeac35f94
Stephan Preibisch <stephan.preibisch@gmx.de>
2026-08-26 10:20:47 -0400

    Just sat with @indecisiveuser and discussed artifacts in ClosestPixelWins. We use blending weights to determine distance to the border, which used a 40 pixel border where its weights are 0, thus if two images overlap in those areas, a random (or likely lower viewID) view is chosen. This is fixed here by not having a border. However, inside the view, weights are 1, so they do not encode distance either. This is less likely to happen, but needs to be fixed too.
    This commit may relieve some urgent issues, but this needs more work. We need to replace this. No need to do cosines, just the distance from the center is fine, will be much faster too.

--- src/main/java/net/preibisch/mvrecon/process/fusion/blk/BlkAffineFusion.java
+++ src/main/java/net/preibisch/mvrecon/process/fusion/blk/BlkAffineFusion.java
@@ -226,6 +226,7 @@ public class BlkAffineFusion
 			// instantiate blending if necessary
 			final float[] blending = Util.getArrayFromValue( FusionTools.defaultBlendingRange, 3 );
 			final float[] border = Util.getArrayFromValue( FusionTools.defaultBlendingBorder, 3 );
+			final float[] borderClosestPixelWins = Util.getArrayFromValue( 0f, 3 );
 
 			// adjust both for z-scaling (anisotropy), downsampling, and registrations itself
 			FusionTools.adjustBlending( viewDimensions.get( viewId ), Group.pvid( viewId ), blending, border, model );
@@ -252,7 +253,7 @@ public class BlkAffineFusion
 				break;
 			case CLOSEST_PIXEL_WINS:
 				// we need to use the blending weights, whatever weight is highest wins
-				weights.add( Blending.create( inputImg, border, blending, transform ) );
+				weights.add( Blending.create( inputImg, borderClosestPixelWins, blending, transform ) );
 				break;
 			case AVG_BLEND_CONTENT:
 				final BlockSupplier< FloatType > cb1 = ContentBased.create( inputImg, sigma1, sigma2, ContentBased.defaultScale );
