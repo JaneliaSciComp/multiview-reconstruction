d48dd13aedffd1708144c8f5c08b70845665cf5b
Stephan Preibisch <stephan.preibisch@gmx.de>
2026-09-03 18:10:59 -0400

    put HDF5 support back in

--- src/main/java/net/preibisch/mvrecon/process/n5api/N5ApiTools.java
+++ src/main/java/net/preibisch/mvrecon/process/n5api/N5ApiTools.java
@@ -365,8 +365,6 @@ public class N5ApiTools
 				return "OME-ZARR v3";
 			else if (s.name().equals("ZARR2"))
 				return "OME-ZARR v2 (deprecated)";
-			else if (s.name().equals("HDF5"))
-				return "HDF5 (currently not supported)";
 			else
 				return s.name();
 		}).toArray(String[]::new);
